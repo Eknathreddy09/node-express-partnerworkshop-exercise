817f994777b3cc2d83c05f0a7a50d07ecf08f00d
Eknathreddy09 <eknath.reddy09@gmail.com>
2023-11-21 16:17:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello Partner, you did it :)');
 });
 
 app.get('/health', (req, res) => {
